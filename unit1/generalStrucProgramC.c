c6a92f2f0dda18a7addbbe01d27efa0fe7863f5c
walote <1909123@upy.edu.mx>
2022-01-28 08:40:22 -0600

    estructura general de C

---
+++ unit1/generalStrucProgramC.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//*Global variables*//;
+//Create a variable: tipoDato nameVariable = anyData;
+//dataTypes in C/C++/C#/PHP
+int unEntero = 3;
+float unDecimal = 3.1416;
+char* name = "EduardoMendoza";
+char unCaracter = 'M';
+/////////////////////////////////////////////////////
+
+/// La ejecución del programa ///
+int main(){
+    printf("Hello World! %s\n", name);
+    return 0;
+}
+/////////////////////////////////
